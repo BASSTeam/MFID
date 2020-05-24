ca9c7d7bd5821d4b78939dcf025964f86ac6ef89
KaMeHb-UA <marlock@etlgr.com>
2020-05-25 00:28:20 +0300

    preact

---
+++ components/preact.d.ts
@@ -0,0 +1,5 @@
+import { h, Component, render } from 'preact'
+
+export default h
+
+export { Component, render }
---
+++ components/preact.js
@@ -0,0 +1,5 @@
+import { h, Component, render } from 'https://unpkg.com/preact?module'
+
+export default h
+
+export { Component, render }
